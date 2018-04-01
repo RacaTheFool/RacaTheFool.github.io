353a6a81332791776f3d48908d9f2a1e02ec0c63
RacaTheFool <35540657+RacaTheFool@users.noreply.github.com>
2018-04-01 01:28:34 -0600

    Goodnight

--- indv-website/functions.js
+++ indv-website/functions.js
@@ -1,11 +1,14 @@
 function blogPost() {
-    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>-" + document.getElementById("blogName").value;
+    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>- " + document.getElementById("blogName").value;
 }
 //function blogPost() {
 //    window.alert("Thank you for your submission! After it has been approved it will be posted. This may take a few days.");
 //}
 
 function specialRequest() {
-    
     window.alert("Request Submitted!");
+}
+
+function purchase() {
+    document.getElementById("thankYou").innerHTML = "Thank you for your purchase!";
 }
