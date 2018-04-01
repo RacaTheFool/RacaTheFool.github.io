2b54f19badb3b8057ecb00ad08b7c74f7e45f24c
RacaTheFool <35540657+RacaTheFool@users.noreply.github.com>
2018-03-31 22:39:11 -0600

    onclick

--- indv-website/functions.js
+++ indv-website/functions.js
@@ -1,9 +1,9 @@
-//function blogPost() {
-//    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>" + document.getElementById("blogName").value;
-//}
 function blogPost() {
-    window.alert("Thank you for your submission! After it has been approved it will be posted. This may take a few days.")
+    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>-" + document.getElementById("blogName").value;
 }
+//function blogPost() {
+//    window.alert("Thank you for your submission! After it has been approved it will be posted. This may take a few days.");
+//}
 
 function specialRequest() {
     
