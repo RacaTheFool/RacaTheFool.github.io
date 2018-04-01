c26264c7b248be6a6cfafe85c3241c2012f85e15
RacaTheFool <35540657+RacaTheFool@users.noreply.github.com>
2018-03-31 22:34:12 -0600

    quick fix

--- indv-website/functions.js
+++ indv-website/functions.js
@@ -1,5 +1,8 @@
+//function blogPost() {
+//    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>" + document.getElementById("blogName").value;
+//}
 function blogPost() {
-    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>" + document.getElementById("blogName").value;
+    window.alert("Thank you for your submission! After it has been approved it will be posted. This may take a few days.")
 }
 
 function specialRequest() {
