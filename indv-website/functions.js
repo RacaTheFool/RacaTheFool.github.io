7698a80f42126db05f8590fbde3fd45e635a454e
RacaTheFool <35540657+RacaTheFool@users.noreply.github.com>
2018-03-31 22:20:28 -0600

    Partial Completion

---
+++ indv-website/functions.js
@@ -0,0 +1,8 @@
+function blogPost() {
+    document.getElementById("blogSpace").innerHTML = document.getElementById("blogText").value + "<br>" + document.getElementById("blogName").value;
+}
+
+function specialRequest() {
+    
+    window.alert("Request Submitted!");
+}
